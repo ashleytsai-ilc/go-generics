a4e953985c4c34fadd5e7d8e2966d2df07206546
AshleyTsai <ashley@caijiarudeMacBook-Pro.local>
2024-04-11 22:59:48 +0800

    feat: implement find func with generic

--- sum_test.go
+++ sum_test.go
@@ -2,6 +2,7 @@ package generics
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -52,5 +53,36 @@ func TestReduce(t *testing.T) {
 			return x + y
 		}
 		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(num int) bool {
+			return num%2 == 0
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, firstEvenNumber, 2)
+	})
+
+	type Person struct {
+		Name string
+	}
+	t.Run("find the best programmer", func(t *testing.T) {
+		people := []Person{
+			{Name: "Kent Beck"},
+			{Name: "Martin Fowler"},
+			{Name: "Chris James"},
+		}
+
+		king, found := Find(people, func(p Person) bool {
+			return strings.Contains(p.Name, "Chris")
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, king, Person{Name: "Chris James"})
 	})
 }
